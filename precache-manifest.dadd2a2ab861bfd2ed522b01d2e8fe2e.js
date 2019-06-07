7348b8b26f1d8eb3720457ebc67211bcd686a61b
Olga <Jackiepro23@gmail.com>
2019-06-07 12:51:09 +0300

    Add files via upload

---
+++ precache-manifest.dadd2a2ab861bfd2ed522b01d2e8fe2e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "20c6dd52180e0ea9b13ec2e452792b4b",
+    "url": "/static/media/img_5.20c6dd52.png"
+  },
+  {
+    "revision": "7249656784b42a2f4779",
+    "url": "/static/css/main.62d487ae.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a5ad894887d77716a16b",
+    "url": "/static/js/2.ae800cb4.chunk.js"
+  },
+  {
+    "revision": "3856e30f7bcc02dc020c6ac799a27877",
+    "url": "/static/media/img_1.3856e30f.png"
+  },
+  {
+    "revision": "8bf20d67d5be5bc5e2024de7b0f581f5",
+    "url": "/static/media/img_2.8bf20d67.png"
+  },
+  {
+    "revision": "f9e24aab6824c138fa461fde0cb5e507",
+    "url": "/static/media/img_3.f9e24aab.png"
+  },
+  {
+    "revision": "81b8877993928165bfe84fad02bfb6ab",
+    "url": "/static/media/img_4.81b88779.png"
+  },
+  {
+    "revision": "7249656784b42a2f4779",
+    "url": "/static/js/main.26f0650e.chunk.js"
+  },
+  {
+    "revision": "79ee39d1055f721dafe0aab7f8314a2c",
+    "url": "/static/media/fb.79ee39d1.svg"
+  },
+  {
+    "revision": "e6c009327f5b12bad91dbab730330fbf",
+    "url": "/static/media/tw.e6c00932.svg"
+  },
+  {
+    "revision": "e9995e75e59e8930d025b735ec594ddb",
+    "url": "/static/media/in.e9995e75.svg"
+  },
+  {
+    "revision": "4de0e1bb535f8c76fb31ca3f746b5662",
+    "url": "/static/media/google.4de0e1bb.svg"
+  },
+  {
+    "revision": "be5a3e1f88d26565d26115e9a10af1f0",
+    "url": "/static/media/DecimaNovaProLt.be5a3e1f.otf"
+  },
+  {
+    "revision": "e0e44f630232dfecfeea06e30087c0be",
+    "url": "/static/media/PTSerif.e0e44f63.ttf"
+  },
+  {
+    "revision": "6bce859ba9cbf01a72b90e00ae0310a3",
+    "url": "/index.html"
+  }
+];
